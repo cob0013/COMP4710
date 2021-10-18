c21df49abbd1485c1a8ddca8fc93e04455b7d4cd
Colin Vande Vijvere <60828613+colinVDV@users.noreply.github.com>
2021-10-17 21:01:21 -0500

    added fog
    Added the toggle option in settings and set static fog in the environment

---
+++ Assets/Scripts/FogOption.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOption : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void FogToggle(bool tog)
+    {
+        RenderSettings.fog = tog;
+    }
+
+}
